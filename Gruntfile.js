1ea1c1039c908c14b4af7d848c8857f0fbebd0a8
vinaymavi <vinaymavi@gmail.com>
2016-04-29 15:30:16 +0530

    setup complete with live reloading.

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+/**
+ * Created by vku131 on 4/29/2016.
+ */
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jshint: {
+            options: {
+                reporter: require('jshint-stylish')
+            },
+            build: ['Gruntfile.js', 'handlebars/**/*.js', 'mustache/**/*.js','sass/**/*.js']
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    hostname: 'localhost',
+                    debug: true,
+                    livereload: 35729,
+                    open: true
+                }
+            }
+        },
+        watch: {
+            js: {
+                files: ['handlebars/**/*.js', 'mustache/**/*.js','sass/**/*.js'],
+                task: ['jshint'],
+                options: {
+                    livereload: '<%= connect.server.options.livereload %>'
+                }
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Register Task
+    grunt.registerTask('default', ['jshint','connect','watch:js']);
+};
---
+++ handlebars/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by vku131 on 4/29/2016.
+ */
+var hello = 'look im grunting!';
+
+var awesome = 'yes it is awesome!';
+
